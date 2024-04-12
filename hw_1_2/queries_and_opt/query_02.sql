033f4f575b6be1f851b041426cf851d5a1d48bc9
StefanoF00 <enofel2000@gmail.com>
2024-04-12 11:22:23 +0200

    updated queries 1,2,3,4

--- hw_1_2/queries_and_opt/query_02.sql
+++ hw_1_2/queries_and_opt/query_02.sql
@@ -4,9 +4,13 @@
 for each continent, find number of countries that don't belong to it
 */
 
--- select r.name as continent_name, count(*) as out_countries
--- from continents r, countries c
--- where c.name not in (select co.name
--- 					from countries co join continents re on re.id = co.continent_id
--- 					where re.name = r.name) and c.continent_id is not null
--- group by r.name
+select r.name as continent_name, count(*) as out_countries
+from continents r, countries c
+where c.name not in (
+	select co.name
+ 	from countries co 
+	join continents re on re.id = co.continent_id
+ 	where re.name = r.name
+) 
+and c.continent_id is not null
+group by r.name
--- hw_1_2/queries_and_opt/query_04.sql
+++ hw_1_2/queries_and_opt/query_04.sql
@@ -7,20 +7,26 @@ not autonomous
 -- NON OPTIMIZED
 -- SUBQUERY: this returns the name and currency of the southern cities of each continent
 select distinct c.name as city_name, co.currency_name as currency, sr.name
-from cities c join countries co on c.country_id = co.id join subcontinents sr
-		on co.subcontinent_id = sr.id
+from cities c 
+join countries co on c.country_id = co.id 
+join subcontinents sr on co.subcontinent_id = sr.id
 where sr.name like 'South%'
 
 intersect
 
 -- SUBQUERY: this returns the name and currency of the cities that are not autonomous
 select distinct c.name as city_name, co.currency_name as currency, sr.name
-from cities c join states s on c.state_id = s.id join countries co on co.id = c.country_id
- 	join subcontinents sr on co.subcontinent_id = sr.id
+from cities c 
+join states s on c.state_id = s.id 
+join countries co on co.id = c.country_id
+join subcontinents sr on co.subcontinent_id = sr.id
 where s.type not like 'autonomous%'
 
 -- OPTIMIZED (not intersection)
 select distinct c.name as city_name, co.currency_name as currency, sr.name
-from cities c join countries co on c.country_id = co.id join subcontinents sr 
-	on co.subcontinent_id = sr.id join states s on c.state_id = s.id
-where s.type not like 'autonomous%' and sr.name like 'South%'
+from cities c 
+join countries co on c.country_id = co.id 
+join subcontinents sr on co.subcontinent_id = sr.id 
+join states s on c.state_id = s.id
+where s.type not like 'autonomous%' 
+and sr.name like 'South%'
